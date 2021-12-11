51cd9bf13a2644d68c0706483a2d05060dc047f0
Hisham Khartoum <hishamfire@gmail.com>
2021-12-11 15:19:12 +0300

    Multi Language Portlet

---
+++ src/main/java/multi/language/constants/MultiLanguagePortletKeys.java
@@ -0,0 +1,11 @@
+package multi.language.constants;
+
+/**
+ * @author hdala
+ */
+public class MultiLanguagePortletKeys {
+
+	public static final String MULTILANGUAGE =
+		"multi_language_MultiLanguagePortlet";
+
+}
---
+++ src/main/java/multi/language/portlet/MultiLanguagePortlet.java
@@ -0,0 +1,30 @@
+package multi.language.portlet;
+
+import multi.language.constants.MultiLanguagePortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author hdala
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=MultiLanguage",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + MultiLanguagePortletKeys.MULTILANGUAGE,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class MultiLanguagePortlet extends MVCPortlet {
+}
